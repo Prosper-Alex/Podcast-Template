273231d6da66a0476b38c7ea6e245443c631fd95
Prosper Alex <167307872+Prosper-Alex01010@users.noreply.github.com>
2025-04-21 20:54:59 +0100

    Enhance dropdown menu functionality to support active state toggling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
   if (!isDropdownButton && e.target.closest("[data-dropdowwn]") != null) return;
+
+  let currentDropdown;
+
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdowwn]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdowwn].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
 });
